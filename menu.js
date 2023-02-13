33d5ab3848d00d2ba7a878736a37bd7b9c30714b
tanishapatil1234 <tanishapatil07@gmail.com>
2023-02-13 15:03:39 -0800

    del js file

--- menu.js
+++
@@ -1,9 +0,0 @@
-async function fillTable(url, table) {
-    const tableBody = table.querySelector("tbody");
-    const response = await fetch(url);
-    const data = await response.json();
-
-    console.log(data);
-}
-
-fillTable("http://localhost:5000/", document.querySelector("table"));
